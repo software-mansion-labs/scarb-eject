a0e4c9cc8b69537ad35f788144ee1491ae42df79
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-10-29 15:51:58 +0100

    Add dependency ejection and `CrateSettings` compatibility
    - Implemented dependency ejection within `ProjectConfigContent` using `AllCratesConfig`
    - Add `--no-deps` flag to optionally skip dependencies
    - Created utility functions for converting Scarb metadata to Cairo-project compatible config:
      - `scarb_package_edition`
      - `scarb_cfg_set_to_cairo`
      - `scarb_package_experimental_features`

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+mod utils;
+
+use std::collections::BTreeMap;
 use std::fs;
 use std::path::{Path, PathBuf};
 
 use anyhow::{anyhow, Result};
-use cairo_lang_project::ProjectConfigContent;
+use cairo_lang_project::{AllCratesConfig, ProjectConfigContent};
+use cairo_lang_filesystem::db::{CrateSettings, DependencySettings, Edition};
+use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use clap::Parser;
 use scarb_ui::args::PackagesFilter;
+use crate::utils::{scarb_cfg_set_to_cairo, scarb_package_edition, scarb_package_experimental_features};
 
 #[derive(Parser, Clone, Debug)]
 #[command(about, author, version)]
@@ -17,6 +23,9 @@ struct Args {
 
     #[command(flatten)]
     packages_filter: PackagesFilter,
+
+    #[arg(long)]
+    no_deps: bool,
 }
 
 fn main() -> Result<()> {
@@ -52,7 +61,47 @@ fn main() -> Result<()> {
         .map(|c| (c.name.clone().into(), c.source_root().into()))
         .collect();
 
-    let project_config = ProjectConfigContent { crate_roots };
+    let edition = scarb_package_edition(&Some(&main_package), main_package.name.as_str());
+    let version = main_package.version.clone();
+    let cfg_set = scarb_cfg_set_to_cairo(&compilation_unit.cfg, main_package.name.as_str());
+
+    let dependencies = if args.no_deps {
+        BTreeMap::new()
+    } else {
+        compilation_unit
+            .components
+            .iter()
+            .filter(|c| c.name != "core")
+            .map(|c| {
+                (
+                    c.name.clone().into(),
+                    DependencySettings {
+                        discriminator: c.discriminator.clone().map(|d| d.into()),
+                    }
+                )
+            })
+            .collect()
+    };
+
+    let experimental_features = scarb_package_experimental_features(&Some(&main_package));
+
+    let crate_settings = CrateSettings {
+        edition,
+        version: Some(version),
+        cfg_set,
+        dependencies,
+        experimental_features,
+    };
+
+    let crates_config = AllCratesConfig {
+        global: crate_settings,
+        override_map: OrderedHashMap::default(),
+    };
+
+    let project_config = ProjectConfigContent {
+        crate_roots,
+        crates_config,
+    };
 
     let mut cairo_project_toml = toml::to_string_pretty(&project_config)?;
     cairo_project_toml.push('\n');
---
+++ src/utils.rs
@@ -0,0 +1,52 @@
+use anyhow::Context;
+use cairo_lang_filesystem::db::{Edition, ExperimentalFeaturesConfig};
+use scarb_metadata::PackageMetadata;
+use tracing::{debug, error, warn};
+
+/// Get the [`Edition`] from [`PackageMetadata`], or assume the default edition.
+pub fn scarb_package_edition(package: &Option<&PackageMetadata>, crate_name: &str) -> Edition {
+    package
+        .and_then(|p| p.edition.clone())
+        .and_then(|e| {
+            serde_json::from_value(e.into())
+                .with_context(|| format!("failed to parse edition of package: {crate_name}"))
+                .inspect_err(|e| warn!("{e:?}"))
+                .ok()
+        })
+        .unwrap_or_default()
+}
+
+/// Convert a slice of [`scarb_metadata::Cfg`]s to a [`cairo_lang_filesystem::cfg::CfgSet`].
+///
+/// The conversion is done the same way as in Scarb (except no panicking):
+/// <https://github.com/software-mansion/scarb/blob/9fe97c8eb8620a1e2103e7f5251c5a9189e75716/scarb/src/ops/metadata.rs#L295-L302>
+pub fn scarb_cfg_set_to_cairo(
+    cfg_set: &[scarb_metadata::Cfg],
+    crate_name: &str,
+) -> Option<cairo_lang_filesystem::cfg::CfgSet> {
+    serde_json::to_value(cfg_set)
+        .and_then(serde_json::from_value)
+        .with_context(|| {
+            format!(
+                "scarb metadata cfg did not convert identically to cairo one for crate: \
+                 {crate_name}"
+            )
+        })
+        .inspect_err(|e| warn!("{e:?}"))
+        .ok()
+}
+
+/// Get [`ExperimentalFeaturesConfig`] from [`PackageMetadata`] fields.
+pub fn scarb_package_experimental_features(
+    package: &Option<&PackageMetadata>,
+) -> ExperimentalFeaturesConfig {
+    let contains = |feature: &str| -> bool {
+        let Some(package) = package else { return false };
+        package.experimental_features.iter().any(|f| f == feature)
+    };
+
+    ExperimentalFeaturesConfig {
+        negative_impls: contains("negative_impls"),
+        coupons: contains("coupons"),
+    }
+}
