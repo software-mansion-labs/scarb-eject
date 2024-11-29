a6fa38a9280083e138bf1468405a132d24335670
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2024-11-29 19:28:49 +0100

    Support ejecting dependencies and latest `ProjectConfig` (#1)
    * Update `scarb-metadata` to `1.12.0`; Add `scarb-ui`
    
    * Add dependency ejection and `CrateSettings` compatibility
    
    - Implemented dependency ejection within `ProjectConfigContent` using `AllCratesConfig`
    - Add `--no-deps` flag to optionally skip dependencies
    - Created utility functions for converting Scarb metadata to Cairo-project compatible config:
      - `scarb_package_edition`
      - `scarb_cfg_set_to_cairo`
      - `scarb_package_experimental_features`
    
    * Move `crate_roots`
    
    * Add override_map to crates_config
    
    * Remove utils.rs; Move helpers to main.rs
    
    * Remove `--no-deps` flag
    
    * Rewrite for readability; Set cairo to cc5bd37096cc67d910509738908b6ac26b12f893
    
    * Misc format and refactor
    
    * Use `CORELIB_CRATE_NAME`
    
    * Misc format and refactor
    
    * Adjust deps
    
    * Run clippy; Remove `rust-version` requirement
    
    * Misc format and refactor
    
    * Update cairo to `2.9.1`
    
    * Update src/main.rs
    
    Signed-off-by: maciektr <mtratnowiecki@gmail.com>
    
    * Update src/main.rs
    
    Signed-off-by: maciektr <mtratnowiecki@gmail.com>
    
    ---------
    
    Signed-off-by: maciektr <mtratnowiecki@gmail.com>
    Co-authored-by: maciektr <mtratnowiecki@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use anyhow::{anyhow, Result};
-use cairo_lang_project::ProjectConfigContent;
+use anyhow::{anyhow, Context, Result};
+use cairo_lang_filesystem::cfg::CfgSet;
+use cairo_lang_filesystem::db::{
+    CrateIdentifier, CrateSettings, DependencySettings, Edition, ExperimentalFeaturesConfig,
+    CORELIB_CRATE_NAME,
+};
+use cairo_lang_project::{AllCratesConfig, ProjectConfigContent};
+use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use clap::Parser;
-use scarb_metadata::packages_filter::PackagesFilter;
+use scarb_metadata::{
+    CompilationUnitComponentDependencyMetadata, CompilationUnitComponentMetadata,
+    CompilationUnitMetadata, Metadata, PackageMetadata,
+};
+use scarb_ui::args::PackagesFilter;
+use tracing::warn;
 
 #[derive(Parser, Clone, Debug)]
 #[command(about, author, version)]
@@ -28,31 +39,7 @@ fn main() -> Result<()> {
 
     let main_package = args.packages_filter.match_one(&metadata)?;
 
-    let compilation_unit = metadata
-        .compilation_units
-        .iter()
-        .filter(|unit| unit.package == main_package.id)
-        .min_by_key(|unit| match unit.target.name.as_str() {
-            name @ "starknet-contract" => (0, name),
-            name @ "lib" => (1, name),
-            name => (2, name),
-        })
-        .ok_or_else(|| {
-            anyhow!(
-                "could not find a compilation unit suitable for ejection for \
-                package {}",
-                main_package.id
-            )
-        })?;
-
-    let crate_roots = compilation_unit
-        .components
-        .iter()
-        .filter(|c| c.name != "core")
-        .map(|c| (c.name.clone().into(), c.source_root().into()))
-        .collect();
-
-    let project_config = ProjectConfigContent { crate_roots };
+    let project_config = get_project_config(&metadata, &main_package)?;
 
     let mut cairo_project_toml = toml::to_string_pretty(&project_config)?;
     cairo_project_toml.push('\n');
@@ -72,4 +59,201 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn get_project_config(
+    metadata: &Metadata,
+    main_package: &PackageMetadata,
+) -> Result<ProjectConfigContent> {
+    let compilation_unit = metadata
+        .compilation_units
+        .iter()
+        .filter(|unit| unit.package == main_package.id)
+        .min_by_key(|unit| match unit.target.name.as_str() {
+            name @ "starknet-contract" => (0, name),
+            name @ "lib" => (1, name),
+            name => (2, name),
+        })
+        .ok_or_else(|| {
+            anyhow!(
+                "could not find a compilation unit suitable for ejection for package {}",
+                main_package.id
+            )
+        })?;
+
+    let crate_roots = get_crate_roots(compilation_unit);
+    let crates_config = get_crates_config(metadata, compilation_unit, main_package);
+
+    Ok(ProjectConfigContent {
+        crate_roots,
+        crates_config,
+    })
+}
+
+fn get_crate_roots(
+    compilation_unit: &CompilationUnitMetadata,
+) -> OrderedHashMap<CrateIdentifier, PathBuf> {
+    compilation_unit
+        .components
+        .iter()
+        .filter(|c| c.name != CORELIB_CRATE_NAME)
+        .map(|c| {
+            (
+                c.id.clone().unwrap_or(c.name.clone().into()).into(),
+                c.source_root().into(),
+            )
+        })
+        .collect()
+}
+
+fn get_crates_config(
+    metadata: &Metadata,
+    compilation_unit: &CompilationUnitMetadata,
+    main_package: &PackageMetadata,
+) -> AllCratesConfig {
+    let global_crate_settings = get_global_crate_settings(compilation_unit, main_package);
+    let override_map = compilation_unit
+        .components
+        .iter()
+        .filter(|c| c.name != CORELIB_CRATE_NAME)
+        .map(|component| {
+            (
+                component
+                    .id
+                    .clone()
+                    .unwrap_or(component.name.clone().into())
+                    .into(),
+                get_crate_settings_for_component(component, compilation_unit, metadata),
+            )
+        })
+        .collect();
+
+    AllCratesConfig {
+        global: global_crate_settings,
+        override_map,
+    }
+}
+
+fn get_global_crate_settings(
+    compilation_unit: &CompilationUnitMetadata,
+    package: &PackageMetadata,
+) -> CrateSettings {
+    let edition = get_edition(&Some(package), package.name.as_str());
+    let version = package.version.clone();
+    let cfg_set = get_cairo_cfg_set(&compilation_unit.cfg, package.name.as_str());
+    let dependencies = compilation_unit
+        .components
+        .iter()
+        .filter(|c| c.name != CORELIB_CRATE_NAME)
+        .map(|c| {
+            (
+                c.name.clone(),
+                DependencySettings {
+                    discriminator: c.discriminator.clone().map(Into::into),
+                },
+            )
+        })
+        .collect();
+    let experimental_features = get_experimental_features(Some(package));
+
+    CrateSettings {
+        name: None,
+        edition,
+        version: Some(version),
+        cfg_set,
+        dependencies,
+        experimental_features,
+    }
+}
+
+fn get_crate_settings_for_component(
+    component: &CompilationUnitComponentMetadata,
+    compilation_unit: &CompilationUnitMetadata,
+    metadata: &Metadata,
+) -> CrateSettings {
+    let package = metadata
+        .packages
+        .iter()
+        .find(|package| package.id == component.package);
+    let edition = get_edition(&package, component.name.as_str());
+    let version = package.map(|p| p.version.clone());
+    let cfg_set = component
+        .cfg
+        .as_ref()
+        .and_then(|cfg| get_cairo_cfg_set(cfg, component.name.as_str()));
+    let dependencies = component
+        .dependencies
+        .as_ref()
+        .unwrap_or(&vec![])
+        .iter()
+        .filter_map(|CompilationUnitComponentDependencyMetadata { id, .. }| {
+            compilation_unit
+                .components
+                .iter()
+                .filter(|c| c.name != CORELIB_CRATE_NAME)
+                .find(|c| c.id.as_ref() == Some(id))
+                .map(|c| {
+                    (
+                        c.name.clone(),
+                        DependencySettings {
+                            discriminator: c.discriminator.clone().map(Into::into),
+                        },
+                    )
+                })
+        })
+        .collect();
+    let experimental_features = get_experimental_features(package);
+
+    CrateSettings {
+        name: Some(component.name.clone().into()),
+        edition,
+        version,
+        cfg_set,
+        dependencies,
+        experimental_features,
+    }
+}
+
+/// Get the [`Edition`] from [`PackageMetadata`], or assume the default edition.
+fn get_edition(package: &Option<&PackageMetadata>, crate_name: &str) -> Edition {
+    package
+        .and_then(|p| p.edition.clone())
+        .and_then(|e| {
+            serde_json::from_value(e.into())
+                .with_context(|| format!("failed to parse edition of package: {crate_name}"))
+                .inspect_err(|e| warn!("{e:?}"))
+                .ok()
+        })
+        .unwrap_or_default()
+}
+
+/// Convert a slice of [`scarb_metadata::Cfg`]s to a [`cairo_lang_filesystem::cfg::CfgSet`].
+///
+/// The conversion is done the same way as in Scarb (except no panicking):
+/// <https://github.com/software-mansion/scarb/blob/9fe97c8eb8620a1e2103e7f5251c5a9189e75716/scarb/src/ops/metadata.rs#L295-L302>
+fn get_cairo_cfg_set(cfg_set: &[scarb_metadata::Cfg], crate_name: &str) -> Option<CfgSet> {
+    serde_json::to_value(cfg_set)
+        .and_then(serde_json::from_value)
+        .with_context(|| {
+            format!(
+                "scarb metadata cfg did not convert identically to cairo one for crate: {crate_name}"
+            )
+        })
+        .inspect_err(|e| warn!("{e:?}"))
+        .ok()
+}
+
+/// Get [`ExperimentalFeaturesConfig`] from [`PackageMetadata`] fields.
+fn get_experimental_features(package: Option<&PackageMetadata>) -> ExperimentalFeaturesConfig {
+    let contains = |feature: &str| -> bool {
+        package
+            .map(|p| p.experimental_features.contains(&feature.into()))
+            .unwrap_or(false)
+    };
+
+    ExperimentalFeaturesConfig {
+        negative_impls: contains("negative_impls"),
+        associated_item_constraints: contains("associated_item_constraints"),
+        coupons: contains("coupons"),
+    }
 }
